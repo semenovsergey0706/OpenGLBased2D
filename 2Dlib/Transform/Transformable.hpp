5afa2b93419cde29b8bcd75ebdb698753998d01b
Semenov Sergey <semenovsergey0706@gmail.com>
2020-11-07 14:37:34 +0200

    delete unnecessary

--- 2Dlib/Transform/Transformable.hpp
+++ 2Dlib/Transform/Transformable.hpp
@@ -48,31 +48,3 @@ public:
 	friend TDEStorage;
 };
 
-/*class Transformable : public BaseTransformable
-{
-protected:
-	bool m_transformUpdate = false;
-	bool m_invTransformUpdate = false;
-
-	void upUpdateFlag();
-	void updateTransform();
-	void updateITransform();
-public:
-	Transformable();
-	void setOrigin(float x, float y);
-	void setOrigin(const glm::vec2& pos);
-	void setPosition(float x, float y);
-	void setPosition(const glm::vec2& pos);
-	void setScale(float x, float y);
-	void setScale(const glm::vec2& pos);
-	void setRotation(float angle);
-
-	void move(float x, float y);
-	void move(const glm::vec2& pos);
-	void scale(float x, float y);
-	void scale(const glm::vec2& pos);
-	void rotate(float angle);
-	virtual ~Transformable() = default;
-
-	friend TDEStorage;
-};*/
